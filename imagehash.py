935fcc2ac8597c9e84e097ef835913ff2d2bb1c3
K. Kharchenko <konst1970@gmail.com>
2018-09-21 22:37:23 +0300

    myhash.py added

--- imagehash.py
+++
@@ -1,8 +0,0 @@
-# image hash for calculating some hash value for given image
-
-def hash(n):
-  print(n)
-
-if __name__ == "__main__":
-    import sys
-    hash(sys.argv[1])
---
+++ myhash.py
@@ -0,0 +1,19 @@
+#
+# Image hash for calculating some hash value for given image
+# 
+#   to use this file type
+# 
+#   pip install ImageHash
+#
+
+from PIL import Image
+import imagehash
+
+def myhash(file):
+  hash = imagehash.average_hash(Image.open(file))
+  return hash
+
+if __name__ == "__main__":
+    import sys
+    hash = myhash(sys.argv[1])
+    print(sys.argv[1], hash)

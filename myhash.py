55301c7a042e45a079e97ca858f8e2b66c002c45
K. Kharchenko <konst1970@gmail.com>
2018-09-21 22:44:42 +0300

    myhash.py for two variants

--- myhash.py
+++ myhash.py
@@ -13,7 +13,16 @@ def myhash(file):
   hash = imagehash.average_hash(Image.open(file))
   return hash
 
+def myhashimg(img):
+  hash = imagehash.average_hash(img)
+  return hash
+
+
 if __name__ == "__main__":
     import sys
     hash = myhash(sys.argv[1])
     print(sys.argv[1], hash)
+
+    hash1 = myhashimg (Image.open(sys.argv[1]))
+    print(sys.argv[1], hash1)
+

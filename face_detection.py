2e21bab524969f99d8dd94da48783eadb6da91fd
NnataKha <39153381+NnataKha@users.noreply.github.com>
2018-10-19 15:45:21 +0300

    issue #23
    Calculating dependancy of the number of detected faces on the size of the image

--- face_detection.py
+++ face_detection.py
@@ -1,27 +1,40 @@
-import cv2
-face_cascade = cv2.CascadeClassifier('C:\\Users\\mika\\Anaconda3\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+from cv2 import CascadeClassifier, imread, cvtColor, COLOR_BGR2GRAY, rectangle, resize
+face_cascade = CascadeClassifier('C:\\Users\\mika\\Anaconda3\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
 
 def read(file):
-    return cv2.imread(file)
+    return imread(file)
 
 def face_detect(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 3)
+    gray = cvtColor(img, COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.03, minNeighbors=3, minSize=(60, 60), maxSize=(130, 130))
     for (x,y,w,h) in faces:
-        cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),1)
+        rectangle(gray,(x,y),(x+w,y+h),(255,0,0),1)
     return gray
 
 def is_face(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    return not faces is ()
+    gray = cvtColor(img, COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.05, minNeighbors=5, minSize=(60, 60))
+    return not (faces is ())
+
+def is_face_gs(img):
+    faces = face_cascade.detectMultiScale(img, scaleFactor = 1.05, minNeighbors=5, minSize=(60, 60))
+    return not (faces is ())
+
+def is_face_sz(img, min_s):
+    faces = face_cascade.detectMultiScale(img, scaleFactor = 1.05, minNeighbors=5, minSize=(min_s, min_s))
+    return not (faces is ())
 
 def crop(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 3)
-    for (x,y,w,h) in faces:
-        cropped_img = gray[y:y+h, x:x+w]
-    return cropped_img  
+    gray = cvtColor(img, COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.03, minNeighbors=5, minSize=(60, 60), maxSize=(130, 130))
+    res = ()
+    if len(faces)>0:
+        for (x,y,w,h) in faces:
+            cropped_img = gray[y:y+h, x:x+w]
+            if is_face_gs(cropped_img):
+                res = cropped_img
+    return res
+
 
 if __name__ == "__main__":
     import sys
